21d2032d620aaa81a6862b8d4d26f93c410f8c71
Mohan@360ithub <mohan@360ithub.com>
2024-05-06 18:17:33 +0530

    from local 6.5.24

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -95,7 +95,7 @@ class Customer(TransactionBase):
 		portal_users: DF.Table[PortalUser]
 		primary_address: DF.Text | None
 		represents_company: DF.Link | None
-		sales_person: DF.Link | None
+		sales_person: DF.Link
 		sales_person_mail_id: DF.Data | None
 		sales_team: DF.Table[SalesTeam]
 		salutation: DF.Link | None
